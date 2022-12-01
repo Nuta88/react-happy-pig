9b5a470f7d2fbb851e3e342c240abd6486e590ef
Anna Wagner <annwagner8888@gmail.com>
2022-12-01 14:16:07 +0200

    HP6: Add ErrorBoundary

---
+++ src/components/ErrorBoundary/index.jsx
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {}
+
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
