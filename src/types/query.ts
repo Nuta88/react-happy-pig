6a884c2856c83b9c9c3a224d3940234133f5794d
Anna Wagner <annwagner8888@gmail.com>
2023-03-06 22:00:26 +0200

    HP-52: Update useUpdateFund hook

---
+++ src/types/query.ts
@@ -0,0 +1,11 @@
+export interface GeneralQueryProps {
+  isUninitialized: boolean;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  error?: any
+}
+
+export interface MutationResult extends GeneralQueryProps {
+  reset: () => void
+}
