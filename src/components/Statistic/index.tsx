18401a42d0d102642737ea8e1fac762d82bc9e09
Anna Wagner <annwagner8888@gmail.com>
2026-01-08 16:20:04 +0200

    HP-144: fix Statistic style

--- src/components/Statistic/index.tsx
+++ src/components/Statistic/index.tsx
@@ -1,5 +1,16 @@
 import { Statistic as AntdStatistic, StatisticProps } from 'antd';
+import styled from 'styled-components';
 
-export const Statistic = (props: StatisticProps): JSX.Element => (
-  <AntdStatistic {...props} />
+const StatisticStyled = styled(AntdStatistic)<{ bodyColor: string | undefined }>`
+    .ant-statistic-content {
+        color: ${props => props.bodyColor ?? 'inherit'};
+    }
+`;
+
+interface StyledStatisticProps extends StatisticProps {
+  bodyColor?: string
+}
+
+export const Statistic = ({ bodyColor, ...props }: StyledStatisticProps): JSX.Element => (
+  <StatisticStyled bodyColor={bodyColor} {...props} />
 );
